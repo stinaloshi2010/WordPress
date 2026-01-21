16cf7170af0b27d6ae5576ff0b2158639809274e
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-01-21 20:21:32 +0100

    New Changes

--- functions.php
+++ functions.php
@@ -9,6 +9,8 @@ function load_scripts(){
 add_action('wp_enqueue_scripts','load_scripts');
 
 function config(){
+
+    register_nav_menus(
     array(
         'wp_devs_main_menu' => 'Mian Menu',
         'wp_devs_footer_menu' => 'Footer Menu',
@@ -16,7 +18,26 @@ function config(){
     ));
 }
 
-add_action('after-setup_theme', 'config',0);
+$args = array(
+    'width' => 225,
+    'height' => 1920
+);
+
+add_theme_support('custom-header',$args);
+add_theme_support('post-thumbnails');
+add_theme_support('custom-logo', array(
+    'height' => 110,
+    'width' => 200,
+    'flex-height' => true,
+    'flex-width' => true
+));
+
+add_theme_support('automatic-feed-links');
+add_theme_support('htmls',array('comment-list','comment-form', 'search-form','gallery','caption','style','script'));
+add_theme_support('title-tag');
+
+
+add_action('after_setup_theme','config',0);
 
 
 ?>
