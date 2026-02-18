3557acdb0d6d1cabc956ab6dbcb67f4f184859fc
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-02-18 20:18:00 +0100

    error

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 function load_scripts(){
+
     wp_enqueue_style('style',get_template_directory_uri()."/style.css", false,'1.1', 'all');
     wp_enqueue_script('main',get_template_directory_uri()."/js/main.js",array(),'1.0',true);
-
 
 }
 
